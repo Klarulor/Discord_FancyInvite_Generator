eb819e4097132c6b1f610ee23345b61da9615449
Celenovich Klarulor <klarulor@gmail.com>
2022-04-10 14:24:52 +0300

    Mass update

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,8 @@ const guildId: string = process.env.GUILD ?? "<GuildID>>";
 const channelID: string = process.env.CHANNEL ?? "<ChannelID>>";
 const inviteMaxLength: number = 10;
 const repeatSymbolCount: number = 1;
-const repeatEachSymbolCount: number = 5;
-
+const repeatEachSymbolCount: number = 4;
+const needInvites: number = 999;
 
 
 
@@ -25,26 +25,40 @@ client.on(`ready`, async () => {
     startCycle();
 });
 let attempts: number = 0;
+let founds: number = 0;
+const keys: string[] = [];
 function startCycle(){
     const interval = setInterval(async () => {
-        attempts++;
         const invite = await createInvite();
         const code = invite.code.split('gg/')[1]?.toLowerCase() ?? invite.code.toLowerCase();
         const response = checkCode(code);
+        attempts++;
         if(response.success){
             clearInterval(interval);
+            keys.push(code);
+            founds++;
+            attempts = 0;
             console.log(`----------------------------------------------- FOUND!!!!\n${invite.code}`);
+            if(founds >= needInvites){
+                console.log("Exit >>",
+                    keys);
+            }else
+                startCycle();
         }else{
-            console.log(`${attempts}.) ${response.count} ${response.repeat} ${invite}`);
+            console.log(`${attempts}.) ${response.diagnostic.count} ${response.diagnostic.repeat} ${invite} ${founds}`);
             invite.delete();
         }
-    }, 3000);
+    }, 4000);
 }
 function checkCode(code: string): ICheckingResponse{
     const response: ICheckingResponse = {
         success: false,
         repeat: 0,
-        count: 0
+        count: 0,
+        diagnostic: {
+            repeat: 0,
+            count: 0
+        }
     };
     const symbols: any = {};
     for(let i = 0; i < code.length; i++){
@@ -54,13 +68,17 @@ function checkCode(code: string): ICheckingResponse{
         else symbols[char]++;
     }
     for(let k in symbols){
-        if(symbols[k] > 1)
+        if(symbols[k] > 1){
+            response.diagnostic.repeat += symbols[k] -1;
+            response.diagnostic.count++;
+        }
+        if(symbols[k] >= repeatEachSymbolCount)
         {
-            response.repeat += symbols[k] - 1;
+            response.repeat += symbols[k];
             response.count++;
         }
     }
-    if(code.length <= inviteMaxLength && response.count >= repeatSymbolCount && response.repeat >= repeatEachSymbolCount)
+    if(code.length <= inviteMaxLength && response.count >= repeatSymbolCount)
         response.success = true;
     return response;
 }
@@ -74,6 +92,11 @@ async function createInvite(): Promise<Invite>{
 
 interface ICheckingResponse{
     success: boolean;
+    repeat: number;
+    count: number;
+    diagnostic: IDiagnosticInformation;
+}
+interface IDiagnosticInformation{
     repeat: number;
     count: number;
 }
